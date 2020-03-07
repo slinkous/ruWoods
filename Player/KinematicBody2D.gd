957e953c850b5a5c81fd0ef759987df1cc769274
MB129USER <MB129USER@StudentLabs.edu>
2020-03-07 14:27:25 -0800

    Add new isotiles and arrange scenes and tilesets into folders

--- Player/KinematicBody2D.gd
+++ Player/KinematicBody2D.gd
@@ -8,6 +8,10 @@ func get_input():
 	velocity = Vector2()
 	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")	
 	velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	if Input.is_action_pressed("sneak"):
+		velocity *= 0.5
+		
 	velocity *= speed
 
 # Called when the node enters the scene tree for the first time.
