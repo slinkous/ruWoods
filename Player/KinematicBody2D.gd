df1126c1fd27595c0dce8c633f94ae27166916f5
BuildTools <sralinkous@gmail.com>
2020-05-02 13:50:08 -0700

    Commit like a million changes

--- Player/KinematicBody2D.gd
+++ Player/KinematicBody2D.gd
@@ -1,14 +1,24 @@
 extends KinematicBody2D
 
-export (int) var speed = 200;
+export (int) var speed = 64;
 
-var velocity = Vector2()
+var velocity = Vector2.ZERO
+
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func get_input():
-	velocity = Vector2()
+	velocity = Vector2.ZERO
 	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")	
 	velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
+	if velocity != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", velocity.normalized())
+		animationTree.set("parameters/Walk/blend_position", velocity.normalized())
+		animationState.travel("Walk")
+	else: 
+		animationState.travel("Idle")
+		
 	if Input.is_action_pressed("sneak"):
 		velocity *= 0.5
 		
@@ -18,6 +28,7 @@ func get_input():
 func _physics_process(delta):
 	get_input();
 	velocity = move_and_slide(velocity)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
