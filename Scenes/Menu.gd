c4dad4d26769fbdd227663c25042b3bc82ff25eb
BuildTools <sralinkous@gmail.com>
2020-05-16 10:31:52 -0700

    Transition from menu to main scene

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var mainScene = load("res://Scenes/RusWoods.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_input():
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene_to(mainScene)
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	get_input();
